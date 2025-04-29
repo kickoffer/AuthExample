cb41a3f39cecdb1c0f1021764c693cb7634da3ca
heeragi <iju0@aladin.co.kr>
2025-04-29 11:10:13 +0900

    회원가입 로직 구현

--- src/main/kotlin/kr/co/kickoffer/authexample/service/AdminUserServiceImpl.kt
+++ src/main/kotlin/kr/co/kickoffer/authexample/service/AdminUserServiceImpl.kt
@@ -3,9 +3,11 @@ package kr.co.kickoffer.authexample.service
 import kr.co.kickoffer.authexample.model.dto.CreateAdminDto
 import kr.co.kickoffer.authexample.model.entity.Admin
 import kr.co.kickoffer.authexample.repository.AdminRepository
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.UUID
+import javax.security.auth.login.CredentialException
 
 @Service
 class AdminUserServiceImpl(
@@ -13,7 +15,12 @@ class AdminUserServiceImpl(
     private val passwordEncoder: PasswordEncoder
 ): AdminUserService {
 
-    override fun create(email: String, password: String) {
+    override fun create(email: String, password: String) = transaction {
+
+        adminRepository.findByName(email)?.let {
+            throw CredentialException("User already exists")
+        }
+
         adminRepository.create(
             CreateAdminDto(
                 name = email,
